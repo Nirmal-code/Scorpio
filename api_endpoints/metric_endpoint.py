aa6bf5e4594ef2e3f75197412bfd1914c1dbdc03
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2025-11-25 12:07:44 -0500

    test

--- api_endpoints/metric_endpoint.py
+++ api_endpoints/metric_endpoint.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from fastapi import FastAPI, Query, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.params import Header
+from fastapi.responses import JSONResponse
 from endpoint_pipeline.metrics_pipeline import MetricPipeline
 import os
 from dotenv import load_dotenv
@@ -24,4 +25,4 @@ def run_pipeline(api_key: str = Header(None), tickers: Optional[str] = Query(Non
     # Ensure we always work with a list of strings and have a sensible fallback
     tickers_list = str(tickers).strip().split(',') if tickers else []
     results = pipeline.run(tickers_list)
-    return jsonable_encoder({"status": "success", "data": results})
+    return JSONResponse(content=results)
